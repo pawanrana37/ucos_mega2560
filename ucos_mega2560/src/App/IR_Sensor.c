abaf46e7ecb2f0c31b3085693fca1d9f0e4a2e1f
vjyq1x <pawan.singh.rana@aptiv.com>
2022-08-13 23:26:06 +0200

    Restructuere the Code

---
+++ ucos_mega2560/src/App/IR_Sensor.c
@@ -0,0 +1,226 @@
+/*
+*********************************************************************************************************
+*                                           Atmel ATmega256
+*                                           IR_Sensor Decode
+*
+*                                (c) Copyright 2005, Micrium, Weston, FL
+*                                          All Rights Reserved
+*
+*
+* File : IR_Sensor.c
+* By   : Pawan Singh Rana
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                               INCLUDES
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+#include <avr_debugger.h>
+#include "avr8-stub.h"
+
+/*
+*********************************************************************************************************
+*                                               DEFINES
+*********************************************************************************************************
+*/
+#define IR_FRAME_TIME_STARTUP_PLUS_ADDRESS 41u
+#define IR_FRAME_TOTAL_TIME 68u
+/*
+*********************************************************************************************************
+*                                              GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+typedef struct ir_data_struct 
+{
+    /*Capture OS Tick*/
+    volatile unsigned int previous_tick_count;
+    volatile unsigned int current_tick_count;
+
+    volatile unsigned int alive_counter;
+    
+    /*Capture Frame*/
+    volatile unsigned int IR_Command_Sequence; 
+    volatile unsigned int IR_Address_Sequence;
+    volatile unsigned int IR_Startup_Sequence;  
+    volatile unsigned int IR_Start_Frame_Init;
+    volatile unsigned int IR_RepeatCode_Sequence;
+    volatile unsigned int IR_CommandData_Time_Counter; 
+
+    /*Capture Command Data*/
+    volatile unsigned  int ir_raw_data_0;
+
+} ir_strucet_t; 
+
+static ir_strucet_t ir_str_t=   {.previous_tick_count = FALSE,
+                                .current_tick_count = FALSE,
+                                .IR_Command_Sequence = FALSE,
+                                .IR_Address_Sequence = FALSE,
+                                .IR_Startup_Sequence = FALSE,
+                                .IR_Start_Frame_Init = FALSE,
+                                .IR_RepeatCode_Sequence = FALSE,
+                                .IR_CommandData_Time_Counter = FALSE,
+                                .ir_raw_data_0 = 0b0000000
+                                }; 
+
+extern OS_EVENT *msgbox; 
+
+/*
+*********************************************************************************************************
+*                                            FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+static void IR_Retrive_RepeatCode(void);
+static void IR_Calculate_Frame(void);
+static void IR_Retrive_CommandData(void);
+static void IR_Retrive_AddressData(void);
+static void IR_Retrive_StartFrame(void);
+
+/*
+*********************************************************************************************************
+*                                                   IR_Retrive_RepeatCode 
+* Description : This function shall capture the Repeate code of the IR Frame.
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+static void IR_Retrive_RepeatCode(void)
+{
+    if(ir_str_t.IR_RepeatCode_Sequence == FALSE && ir_str_t.IR_Command_Sequence == TRUE && ir_str_t.IR_Address_Sequence == TRUE && ir_str_t.IR_Startup_Sequence == TRUE && ir_str_t.IR_Start_Frame_Init == TRUE)
+    {
+        ir_str_t.current_tick_count = OSTimeGet();
+        if(((ir_str_t.current_tick_count - ir_str_t.previous_tick_count) >= 121))
+        {
+
+            ir_str_t.IR_Start_Frame_Init = FALSE;
+            breakpoint();
+            ir_str_t.ir_raw_data_0 = FALSE;
+        }
+
+    }
+}
+/*
+*********************************************************************************************************
+*                                                   IR_Retrive_CommandData
+* Description : This function capture the 27ms CommandFrame and Extract the Data out of it.
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+static void IR_Retrive_CommandData(void)
+{
+    if(ir_str_t.IR_Command_Sequence == FALSE && ir_str_t.IR_Address_Sequence == TRUE && ir_str_t.IR_Startup_Sequence == TRUE && ir_str_t.IR_Start_Frame_Init == TRUE)
+    {
+        ir_str_t.current_tick_count = OSTimeGet();
+        if(((ir_str_t.current_tick_count - ir_str_t.previous_tick_count) >= (IR_FRAME_TIME_STARTUP_PLUS_ADDRESS + ir_str_t.IR_CommandData_Time_Counter))&& ((ir_str_t.current_tick_count - ir_str_t.previous_tick_count) <= IR_FRAME_TOTAL_TIME))
+        {
+            if (IR_TSOP_SENSOR_PIN_STATUS == FALSE) {
+                ir_str_t.ir_raw_data_0 &= ~(1 << (8 - (ir_str_t.IR_CommandData_Time_Counter/3))); //Clear bit (7-b)
+            } else {
+                ir_str_t.ir_raw_data_0 |= (1 << (8 - (ir_str_t.IR_CommandData_Time_Counter/3))); //Set bit (7-b)
+            } 
+            
+            ir_str_t.IR_CommandData_Time_Counter = ir_str_t.IR_CommandData_Time_Counter +3;
+
+        }
+        else
+        {
+                /**Do Nothing*/
+        }
+        if((ir_str_t.current_tick_count - ir_str_t.previous_tick_count) >=69)
+        {
+            OSMboxPost(msgbox, ir_str_t.ir_raw_data_0);
+            ir_str_t.IR_Command_Sequence = TRUE;
+            ir_str_t.IR_CommandData_Time_Counter = 3;
+            ir_str_t.previous_tick_count = OSTimeGet();
+            ir_str_t.IR_Start_Frame_Init = FALSE;
+        }
+
+    }
+
+}
+/*
+*********************************************************************************************************
+*                                                   IR_Retrive_AddressData
+* Description : This function capture the 27ms Address Frame.
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+static void IR_Retrive_AddressData(void)
+{
+    if(ir_str_t.IR_Address_Sequence == FALSE && ir_str_t.IR_Startup_Sequence == TRUE && ir_str_t.IR_Start_Frame_Init == TRUE)
+    {
+    
+        ir_str_t.current_tick_count = OSTimeGet();
+        if((ir_str_t.current_tick_count - ir_str_t.previous_tick_count) > IR_FRAME_TIME_STARTUP_PLUS_ADDRESS)
+        {
+            ir_str_t.IR_Address_Sequence = TRUE;
+            ir_str_t.IR_Command_Sequence = FALSE;
+
+        }
+    }
+}
+/*
+*********************************************************************************************************
+*                                                   IR_Retrive_StartFrame
+* Description : This function capture the 9ms LOW and 4.5ms HIGH pulse.
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+
+static void IR_Retrive_StartFrame(void)
+{
+    if(ir_str_t.IR_Startup_Sequence == FALSE && ir_str_t.IR_Start_Frame_Init == TRUE)
+    {
+        ir_str_t.current_tick_count = OSTimeGet();
+        if((ir_str_t.current_tick_count - ir_str_t.previous_tick_count) > 14)
+        {
+            ir_str_t.IR_Startup_Sequence = TRUE;
+            ir_str_t.IR_Address_Sequence = FALSE;
+
+        }
+    }
+
+}
+/*
+*********************************************************************************************************
+*                                                   IR_ISR_Init
+* Description : This function capture the Startup IR Frame.
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+static void IR_ISR_Init(void)
+{
+    if(ir_str_t.IR_Start_Frame_Init == FALSE)
+    {
+        ir_str_t.previous_tick_count = OSTimeGet();
+        ir_str_t.IR_Start_Frame_Init = TRUE;
+        ir_str_t.IR_Startup_Sequence = FALSE;
+    }
+
+}
+
+/*
+*********************************************************************************************************
+*                                                   ISR: INT5_vect
+* Description : This ISR is configured to be triggered on the Rising and falling edge of the IR Frame.
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+ISR(INT5_vect)
+{
+    IR_ISR_Init();
+    IR_Retrive_StartFrame();
+    IR_Retrive_AddressData();
+    IR_Retrive_CommandData();
+    // IR_Retrive_RepeatCode();
+}
+
---
+++ ucos_mega2560/src/RTE/Rte.c
@@ -0,0 +1,339 @@
+/*
+*********************************************************************************************************
+*                                           Atmel ATmega256
+*                                               Rte
+*
+*                                (c) Copyright 2005, Micrium, Weston, FL
+*                                          All Rights Reserved
+*
+*
+* File : Rte.c
+* By   : Pawan Singh Rana
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                               INCLUDES
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+#include <avr_debugger.h>
+#include "avr8-stub.h"
+
+/*
+*********************************************************************************************************
+*                                               DEFINES
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                              GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+static OS_STK TaskStartStk[APP_CFG_TASK_START_STK_SIZE];
+static OS_STK TaskStk[APP_CFG_N_TASKS][APP_CFG_TASK_STK_SIZE];
+OS_EVENT *msgbox; 
+OS_EVENT *msgbox1; 
+OS_EVENT *msgbox2;
+/*
+*********************************************************************************************************
+*                                            FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+static void TaskStart(void *p_arg);
+static void TaskStartCreateTasks(void);
+extern void OS_Resource_Init(void);
+extern void OS_Task_Create_Ext(void);
+
+/*
+*********************************************************************************************************
+*                                                 START TASK
+*********************************************************************************************************
+*/
+
+static void TaskStart(void *p_arg)
+{
+    /*STATISTICS INITIALIZATION*/
+    #if (OS_TASK_STAT_EN > 0)
+    OSStatInit();
+    #endif
+
+    /*Create Tasks*/
+    TaskStartCreateTasks();
+
+    /* Delete start task*/
+    OSTaskDel(OS_PRIO_SELF);    
+} 
+
+/*
+*********************************************************************************************************
+*                                                 EXT TASK
+*********************************************************************************************************
+*/
+
+void OS_Task_Create_Ext()
+{
+    /*Create the start task*/
+    OSTaskCreateExt((void (*)(void *)) TaskStart,
+                    (void           *) 0,
+                    (OS_STK         *)&TaskStartStk[APP_CFG_TASK_START_STK_SIZE - 1],
+                    (INT8U           ) APP_CFG_TASK_START_PRIO,
+                    (INT16U          ) APP_CFG_TASK_START_PRIO,
+                    (OS_STK         *)&TaskStartStk[0],
+                    (INT32U          )(APP_CFG_TASK_START_STK_SIZE),
+                    (void           *) 0,
+                    (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));   
+} 
+
+
+/*
+*********************************************************************************************************
+*                                                   OS Resources
+*********************************************************************************************************
+*/
+void OS_Resource_Init()
+{
+
+    /*Message Box Initialization*/
+    msgbox = OSMboxCreate((void *)0);
+    if(msgbox == ((void *)0))
+    {
+        /* Failed to create message box */
+        /* Return error to caller? */
+    } 
+
+    msgbox1 = OSMboxCreate((void *)0);
+    if(msgbox1 == ((void *)0))
+    {
+        /* Failed to create message box */
+        /* Return error to caller? */
+    } 
+
+    msgbox2 = OSMboxCreate((void *)0);
+    if(msgbox2 == ((void *)0))
+    {
+        /* Failed to create message box */
+        /* Return error to caller? */
+    } 
+}
+
+/*
+*********************************************************************************************************
+*                                                   TASKS
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                               TASK_RTE_LED
+*
+* Description : This Task will trigger the Runnables for LED Functionality
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+static void TASK_RTE_LED(void *p_arg)
+{
+    while (1)
+    {
+        PORTJ^=(1<<LED_PIN1);
+        PORTJ^=(1<<LED_PIN0);
+        OSTimeDlyHMSM(0,0,0,100);
+
+    }   
+} 
+/*
+*********************************************************************************************************
+*                                               TASK_RTE_LCD
+*
+* Description : This Task will Trigger the Runnables for LCD Functionality
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+
+// static void TASK_RTE_LCD(void *p_arg)
+// {
+
+//     unsigned char i,a[]={"Anuyukti Kumari"};
+//     Lcd_CmdWrite(0x02);        // Initialize Lcd in 4-bit mode
+//     Lcd_CmdWrite(0x28);        // enable 5x7 mode for chars 
+//     Lcd_CmdWrite(0x01);        // Clear Display
+//     Lcd_CmdWrite(0x0C);        // Display ON, Cursor OFF
+//     Lcd_CmdWrite(0x06);          /*increment cursor (shift cursor to right)*/	
+//     Lcd_CmdWrite(0x80);        // Move the cursor to beginning of first line    */  
+//     // Task body (always written as an infinite loop) 
+//     while(1)
+//     {
+
+//         Lcd_DataWrite('H');
+//         Lcd_DataWrite('e');
+//         Lcd_DataWrite('l');
+//         Lcd_DataWrite('l');
+//         Lcd_DataWrite('o');
+//         Lcd_DataWrite(' ');
+//         // Lcd_DataWrite('w');
+//         // Lcd_DataWrite('o');
+//         // Lcd_DataWrite('r');
+//         // Lcd_DataWrite('l');
+//         // Lcd_DataWrite('d');
+
+//         Lcd_CmdWrite(0xc0);        //Go to Next line and display Good Morning
+//         for(i=0;a[i]!=0;i++)
+//         {
+//             Lcd_DataWrite(a[i]);
+//         }
+//     }  
+        
+// } // Task3()
+/*
+*********************************************************************************************************
+*                                               TASK_RTE_DC_MOTOR
+*
+* Description : This Task will invoke the Runnables for DC Motor
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+// static void TASK_RTE_DC_MOTOR(void *p_arg)
+// {
+
+//     while(1)
+//     {
+//         PORTB|=(1<<DC_MOTOR_IN1);
+//         PORTB&=~(1<<DC_MOTOR_IN2);
+//         OSTimeDlyHMSM(0,0,5,0);
+
+//         PORTB|=(1<<DC_MOTOR_IN2);
+//         PORTB&=~(1<<DC_MOTOR_IN1);
+//         OSTimeDlyHMSM(0,0,5,0);
+//     }   
+   
+// }
+/*
+*********************************************************************************************************
+*                                               TASK_RTE_IR
+*
+* Description : This Task will invoke the IR Runnables
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+
+static void TASK_RTE_IR(void *p_arg)
+{
+    while(1)
+    {
+        OSTimeDlyHMSM(0,0,2,0);
+    }
+
+} 
+
+/*
+*********************************************************************************************************
+*                                               TASK_RTE_UART
+*
+* Description : This Task will invoke the UART Runnables
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+
+static void TASK_RTE_UART(int *p_arg)
+{
+    static unsigned int *ir_received_data=0;
+    INT8U err;
+    while(1)
+    {
+        ir_received_data = OSMboxPend(msgbox, 0, &err);
+
+        switch (err) 
+        { 
+            case OS_ERR_NONE:
+            case OS_ERR_TIMEOUT:
+            case OS_ERR_PEND_ABORT:
+            break;
+            default:
+            break;
+        }
+
+        if (err != OS_ERR_NONE) 
+        { 
+        /* Try again? */
+        } 
+
+        while(!(UCSR0A & (1<<UDRE0)));
+ 
+        UDR0 = (*(unsigned int *)(ir_received_data));
+        breakpoint();
+        OSTimeDlyHMSM(0,0,0,1000);
+    }
+} 
+
+/*
+*********************************************************************************************************
+*                                               TaskStartCreateTasks
+*
+* Description : This function create OS Tasks
+*               
+* Arguments   : none
+*********************************************************************************************************
+*/
+
+void TaskStartCreateTasks(void)
+{
+    OSTaskCreateExt((void (*)(void *)) TASK_RTE_LED,
+                    (void           *) 0,
+                    (OS_STK         *)&TaskStk[0][APP_CFG_TASK_STK_SIZE - 1],
+                    (INT8U           ) 3,
+                    (INT16U          ) 3,
+                    (OS_STK         *)&TaskStk[0][0],
+                    (INT32U          ) APP_CFG_TASK_STK_SIZE,
+                    (void           *) 0,
+                    (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+
+    // OSTaskCreateExt((void (*)(void *)) TASK_RTE_LCD,
+    //                 (void           *) 0,
+    //                 (OS_STK         *)&TaskStk[1][APP_CFG_TASK_STK_SIZE - 1],
+    //                 (INT8U           ) 4,
+    //                 (INT16U          ) 4,
+    //                 (OS_STK         *)&TaskStk[1][0],
+    //                 (INT32U          ) APP_CFG_TASK_STK_SIZE,
+    //                 (void           *) 0,
+    //                 (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+    // OSTaskCreateExt((void (*)(void *)) TASK_RTE_DC_MOTOR,
+    //                 (void           *) 0,
+    //                 (OS_STK         *)&TaskStk[2][APP_CFG_TASK_STK_SIZE - 1],
+    //                 (INT8U           ) 5,
+    //                 (INT16U          ) 5,
+    //                 (OS_STK         *)&TaskStk[2][0],
+    //                 (INT32U          ) APP_CFG_TASK_STK_SIZE,
+    //                 (void           *) 0,
+    //                 (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+    // OSTaskCreateExt((void (*)(void *)) TASK_RTE_IR,
+    //                 (void           *) 0,
+    //                 (OS_STK         *)&TaskStk[1][APP_CFG_TASK_STK_SIZE - 1],
+    //                 (INT8U           ) 4,
+    //                 (INT16U          ) 4,
+    //                 (OS_STK         *)&TaskStk[1][0],
+    //                 (INT32U          ) APP_CFG_TASK_STK_SIZE,
+    //                 (void           *) 0,
+    //                 (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+    // OSTaskCreateExt((void (*)(void *))TASK_RTE_UART,
+    //             (void           *) 0,
+    //             (OS_STK         *)&TaskStk[2][APP_CFG_TASK_STK_SIZE - 1],
+    //             (INT8U           ) 5,
+    //             (INT16U          ) 5,
+    //             (OS_STK         *)&TaskStk[2][0],
+    //             (INT32U          ) APP_CFG_TASK_STK_SIZE,
+    //             (void           *) 0,
+    //             (INT16U          )(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+} 
